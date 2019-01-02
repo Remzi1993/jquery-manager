28466075b5368dff39d160644165e2a5328c4f33
Remzi Cavdar <ict@remzi.info>
2019-01-02 10:38:40 +0100

    Remove folder
    Remove folder to later add this as a submodule

---
+++ index.php
@@ -0,0 +1,1 @@
+<?php // Silence is golden, some servers are configured to show all the folders if there is no index file aka directory listings. This is to prevent that, security through obscurity.
