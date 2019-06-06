5f1bb1bda2f3dbc544d1f4d032705e7b8faeca84
Remzi Cavdar <ict@remzi.info>
2019-06-06 10:00:11 +0200

    Update tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -23,7 +23,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/wp-jquery-manager.php';
+	require dirname( dirname( __FILE__ ) ) . '/jquery-manager.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
